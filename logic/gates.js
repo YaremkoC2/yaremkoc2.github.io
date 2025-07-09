e5888e7445ed77015f39412f0f267f8b6871cfd2
YaremkoC2 <YaremkoC2@gmail.com>
2025-07-09 13:37:12 -0600

    Added some additional properties
    More gate properties that will be needed when they get rendered

--- logic/gates.js
+++ logic/gates.js
@@ -1,19 +1,41 @@
+let nextGateId = 0;
+
 class Gate {
-    constructor(inputA, inputB) {
+    constructor(inputA = null, inputB = null) {
         this.inA = inputA;
         this.inB = inputB;
+        this.x = 0;
+        this.y = 0;
+        this.id = nextGateId++;
+        this.connectedTo = [];
     }
 
-    // Getter for output, calls derived class implementation
     get output() {
         return this.getOutput();
     }
 
-    // Abstract method - should be overridden
     getOutput() {
-        throw new Error('getOutput() must be implemented by subclass');
+        throw new Error('getOutput() must be implemented');
+    }
+
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = 'lightgray';
+        ctx.fillRect(this.x, this.y, 60, 40);
+        ctx.strokeRect(this.x, this.y, 60, 40);
+        ctx.fillStyle = 'black';
+        ctx.fillText(this.constructor.name, this.x + 5, this.y + 25);
+    }
+
+    isInside(x, y) {
+        return x >= this.x && x <= this.x + 60 && y >= this.y && y <= this.y + 40;
     }
 }
+
 
 class And extends Gate {
     getOutput() {
